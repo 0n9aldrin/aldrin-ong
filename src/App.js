877c6d2abc2bb5a9c2faa7095aaafd31e13801f8
Aldrin Ong <0n9aldrin@gmail.com>
2023-08-15 02:56:22 -0700

    Remove base name

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<Router basename="/aldrin-ong">
+			<Router>
 				<Routes>
 					<Route path="/" element={<Homepage />} />
 					<Route path="/about" element={<About />} />
