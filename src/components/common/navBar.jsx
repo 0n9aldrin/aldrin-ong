d788bde8698ddf2e1dd023361884fde7d3a14d4f
Aldrin Ong <0n9aldrin@gmail.com>
2023-08-15 01:29:50 -0700

    fix icon issue

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,38 +1,48 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 import "./styles/navBar.css";
 import INFO from "../../data/user";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = (props) => {
 	const { active } = props;
 
 	const [menuOpen, setMenuOpen] = useState(false); // initially closed
-	const toggleMenu = () => setMenuOpen(prev => !prev);
-	const navItemClass = (current) => `nav-item ${active === current ? "active" : ""}`;
+	const toggleMenu = () => setMenuOpen((prev) => !prev);
+	const navItemClass = (current) =>
+		`nav-item ${active === current ? "active" : ""}`;
 
 	return (
 		<React.Fragment>
 			<div className="nav-container">
 				<nav className="navbar">
-					<div className={`nav-background ${!menuOpen ? "menu" : ""}`}>
+					<div
+						className={`nav-background ${!menuOpen ? "menu" : ""}`}
+					>
 						<ul className={`nav-list ${!menuOpen ? "menu" : ""}`}>
-							{INFO.nav.map(item => (
-								<li key={item.name} className={navItemClass(item.name.toLowerCase())}>
-									<Link to={item.route} onClick={toggleMenu}>{item.name}</Link>
+							{INFO.nav.map((item) => (
+								<li
+									key={item.name}
+									className={navItemClass(
+										item.name.toLowerCase()
+									)}
+								>
+									<Link to={item.route} onClick={toggleMenu}>
+										{item.name}
+									</Link>
 								</li>
 							))}
 						</ul>
 					</div>
 					<button className="menu-btn" onClick={toggleMenu}>
-						{menuOpen ? "✕" : "☰"}
+						<FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
 					</button>
 				</nav>
 			</div>
 		</React.Fragment>
 	);
 };
-
 
 export default NavBar;
