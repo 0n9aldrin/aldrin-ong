c71c785b9ee7bdec23eca4f96e351b560b3f310e
Aldrin Ong <0n9aldrin@gmail.com>
2023-10-11 14:07:26 -0700

    Change article page to route to medium

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -62,7 +62,7 @@ const Articles = () => {
 											date={article().date}
 											title={article().title}
 											description={article().description}
-											link={"/article/" + (index + 1)}
+											link={article().url}
 										/>
 									</div>
 								))}
