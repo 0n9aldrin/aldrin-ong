bc8f68c21895eefa2b67ba65cefcd569135fea3b
Aldrin Ong <0n9aldrin@gmail.com>
2024-03-10 00:37:11 -0800

    Update resume and footer

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
 
 				<div className="footer-credits">
 					<div className="footer-credits-text">
-						© 2023 Aldrin. All Rights Reserved.
+						© 2024 Aldrin. All Rights Reserved.
 					</div>
 				</div>
 			</div>
